e55f6392f9b7cd9917aac7946fc24078f6f75edb
ZanCo21 <fauzan.alghifari21@gmail.com>
2023-07-25 00:44:19 +0700

    fix config

--- app/page.tsx
+++ app/page.tsx
@@ -10,7 +10,6 @@ export default function Home() {
               <div className='flex'>
                   <img src="/profile.png" className='h-72' alt="Picture of the author" /> 
                   <div className='flex-row ps-8'>
-                    {/* <p className='text-cyan-50 font-bold'>Portofolio</p> */}
                     <p className='text-cyan-50 font-bold'>in development</p>
                     <p className='text-cyan-50 text-[82px] font-bold my-[-1rem]'>Fauzan Alghifari</p>
                     <p className='text-cyan-50 pt-4 w-9/12 text-justify font-normal'>Fauzan is a web developer with several years of experience in developing functional websites. He is also open to learning new frameworks and technologies. Fauzan is accustomed to working in development teams and possesses strong communication skills.</p>
@@ -32,16 +31,6 @@ export default function Home() {
           </div>
 
           <div className='ps-8 pt-14'>
-            <table className='text-cyan-50'>
-                <tbody>
-                  {/* <tr>
-                    <th><p>Header 1</p></th>
-                  </tr>
-                  <tr>
-                    <th><p>Header 1</p></th>
-                  </tr> */}
-                </tbody>
-            </table>
           </div>
         </div>
 
