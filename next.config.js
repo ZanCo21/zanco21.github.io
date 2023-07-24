2a0d55f07db320d865695ae7a8ea91d0efab7a9d
ZanCo21 <fauzan.alghifari21@gmail.com>
2023-07-25 00:49:58 +0700

    fix config

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-
+ // Enable static HTML export
+ output: {
+  // Set exportTrailingSlash to true if you want trailing slashes in exported paths.
+  exportTrailingSlash: true,
+},
 }
 
 module.exports = nextConfig
