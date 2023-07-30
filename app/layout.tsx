9e056500f4505939d5bf889d3baaad2456cf9e09
ZanCo21 <fauzan.alghifari21@gmail.com>
2023-07-30 20:51:47 +0700

    project list fix again

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,13 +23,12 @@ export default function RootLayout({
           <link rel="preconnect" href="https://fonts.googleapis.com"/>
           <link rel="preconnect" href="https://fonts.gstatic.com"/>
           <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700&display=swap" rel="stylesheet"/>
-          <script src="/js/jquery.min.js" />
-          <script src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossOrigin="anonymous"></script>
+          <script async  src="/js/jquery.min.js" />
+          <script async src="https://code.jquery.com/jquery-3.7.0.min.js" integrity="sha256-2Pmvv0kuTBOenSvLm6bvfBSSHrUJ+3A7x6P5Ebd07/g=" crossOrigin="anonymous"></script>
         </head>
       <body className={inter.className}>
         {children}
-        <script type='module' src="js/data.js"></script>
-        <script type='"application/javascript' src="js/main.jsx"></script>
+        <script async  type='module' src="js/data.js"></script>
       </body>
     </html>
   )
